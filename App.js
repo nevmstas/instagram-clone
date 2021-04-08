11abfd2a7636271f668046e966eea789d53ddb4d
nevmstas <nevmyvakastas@gmail.com>
2021-04-09 00:14:37 +0600

    set up firebase

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 import LandingScreen from "./components/auth/Landing";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import Register from "./components/auth/Register";
+import * as firebase from "firebase";
 
 const Stack = createStackNavigator();
 
@@ -26,3 +26,16 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBNFGnHDmALa1JIdNrX9VDYo8ivPKVWfxI",
+  authDomain: "instagram-dev-539bc.firebaseapp.com",
+  projectId: "instagram-dev-539bc",
+  storageBucket: "instagram-dev-539bc.appspot.com",
+  messagingSenderId: "467842103391",
+  appId: "1:467842103391:web:da4668e2b20bad5d7c83ea",
+  measurementId: "G-FS54WNM2J7",
+};
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
